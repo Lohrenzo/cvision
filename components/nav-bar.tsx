38e43bda2345918d77e1437147f7471d61215336
Laurence Fubara <laurence.fubara@gmail.com>
2025-08-27 23:36:26 +0100

    Add CVision logo and update NavBar component
    - Introduced a new logo image for CVision.
    - Updated NavBar to display the logo instead of the Binoculars icon.
    - Adjusted styles for the logo in the NavBar.
    - Ensured postinstall script is correctly set in package.json.

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import { Binoculars } from "lucide-react";
 import UserAvatar from "./auth/userAvatar";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function NavBar() {
   const session = await auth();
@@ -14,9 +15,16 @@ export default async function NavBar() {
         <div className="flex items-center gap-3">
           <Link
             href="/"
-            className="p-2 rounded-lg bg-gradient-to-r from-blue-500 to-blue-700"
+            className="rounded-lg bg-gradient-to-r from-blue-500 to-blue-700"
           >
-            <Binoculars className="h-6 w-6 text-white" />
+            {/* <Binoculars className="h-6 w-6 text-white" /> */}
+            <Image
+              src="/cvision-logo.png"
+              width={10}
+              height={10}
+              alt="CVision Logo"
+              className="h-11 w-11 text-white rounded-lg"
+            />
           </Link>
           <div>
             <Link href="/" className="text-2xl font-bold text-white">
